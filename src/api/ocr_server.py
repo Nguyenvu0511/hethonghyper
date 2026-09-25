63663e6d40bf3bf207c611f19667a1b35032727e
Nguyenvu0511 <hoangvu2007qb@gmail.com>
2026-09-25 19:15:01 +0700

    Use ThreadedHTTPServer

--- src/api/ocr_server.py
+++ src/api/ocr_server.py
@@ -48,10 +48,11 @@ class MyHandler(http.server.BaseHTTPRequestHandler):
             self.wfile.write(json.dumps({"error": str(e)}).encode('utf-8'))
             logging.error(f"Loi giai ma: {e}")
 
+class ThreadedHTTPServer(socketserver.ThreadingMixIn, http.server.HTTPServer):
+    allow_reuse_address = True
+
 if __name__ == '__main__':
     PORT = 8080
-    socketserver.TCPServer.allow_reuse_address = True
-    # Rang buoc 0.0.0.0 de IP ben ngoai co the truy cap vao
-    with socketserver.TCPServer(("0.0.0.0", PORT), MyHandler) as httpd:
+    with ThreadedHTTPServer(("0.0.0.0", PORT), MyHandler) as httpd:
         logging.info(f"🚀 ddddocr Private API Server dang chay tai http://0.0.0.0:{PORT}")
         httpd.serve_forever()
